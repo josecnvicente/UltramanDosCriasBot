22bb168021c9c48989932a8b9da1a528c5c3c247
José Carlos <j-rock-fods@hotmail.com>
2024-05-26 18:24:20 -0300

    Padronização program

--- Interfaces/ConsoleApp/Program.cs
+++ Interfaces/ConsoleApp/Program.cs
@@ -6,40 +6,39 @@ using Domain.Interface.Business.Genshin;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ConsoleApp
+namespace ConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            ConfigAppSettings();
+        ConfigAppSettings();
 
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-            var serviceProvider = serviceCollection.BuildServiceProvider();
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
 
-            var botBusiness = serviceProvider.GetService<IBotBusiness>();
+        var botBusiness = serviceProvider.GetService<IBotBusiness>();
 
-            await botBusiness.RunBotAsync();
-        }
+        await botBusiness.RunBotAsync();
+    }
 
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMemoryCache()
-                .AddSingleton<IBotBusiness, BotBusiness>()
-                .AddSingleton<IChoicesBusiness, ChoicesBusiness>()
-                .AddSingleton<IGenshinBusiness, GenshinBusiness>();
-        }
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMemoryCache()
+            .AddSingleton<IBotBusiness, BotBusiness>()
+            .AddSingleton<IChoicesBusiness, ChoicesBusiness>()
+            .AddSingleton<IGenshinBusiness, GenshinBusiness>();
+    }
 
-        private static void ConfigAppSettings()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false);
+    private static void ConfigAppSettings()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
 
-            IConfiguration config = builder.Build();
+        IConfiguration config = builder.Build();
 
-            Configuration.DiscordConfig = config.GetSection("Discord").Get<Configuration.DiscordDto>();
-        }
+        Configuration.DiscordConfig = config.GetSection("Discord").Get<Configuration.DiscordDto>();
     }
 }
